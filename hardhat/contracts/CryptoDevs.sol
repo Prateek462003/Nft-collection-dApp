0955b9513934308c098b2f640a84601fce0c134f
Prateek462003 <prateeksaxena462003@gmail.com>
2023-05-02 09:52:55 +0530

    Added Pause Functionality

--- hardhat/contracts/CryptoDevs.sol
+++ hardhat/contracts/CryptoDevs.sol
@@ -10,6 +10,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     IWhitelist whitelist;
     
     string public baseTokenURI;
+    bool public _paused;
     bool public  preSaleStarted  = false;
     uint256 public preSaleEnded ;
     uint256 public maxTokenIds = 20;
@@ -21,13 +22,18 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         baseTokenURI = baseURI;
         whitelist = IWhitelist(whitelistContract);
     }
+
+    modifier onlyWhenNotPaused{
+        require(!_paused, "Contract currently Paused");
+        _;
+    }
     
     function startPresale() public onlyOwner{
         preSaleStarted = true;
         preSaleEnded = block.timestamp + 5 minutes ;
     }
     
-    function preSaleMint() public payable {
+    function preSaleMint() public payable onlyWhenNotPaused {
         require(preSaleStarted && block.timestamp < preSaleEnded, "PreSale has ended!");
         require(tokenIds < maxTokenIds, "Exceeded the max token Mint Limit");
         require(whitelist.whiteListedAddresses(msg.sender), "You are not whitelisted!");
@@ -37,7 +43,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     }
 
 
-    function publicMint() public payable{
+    function publicMint() public payable onlyWhenNotPaused{
         require(tokenIds < maxTokenIds , "Exceeded the maximum Token Mint Limit");
         require(msg.value >= publicMintPrice, "Insufficient Ethers sent");
         tokenIds++;
@@ -51,6 +57,13 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         require(sent, "Failed To send ether");
     }
 
+    function setPaused(bool val) public onlyOwner{
+        _paused = val;
+    } 
+    
+    function _baseURI() internal view virtual override returns(string memory){
+        return baseTokenURI; 
+    }
     // To recieve ethers....
     receive() external payable {}
     fallback() external payable {}
